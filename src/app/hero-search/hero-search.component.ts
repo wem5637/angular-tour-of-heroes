8a8876490cf36cf2a78e33574917972589a97d18
William McCracken <wmccracken@allegient.com>
2017-11-10 12:40:32 -0500

    revises debounce

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -31,7 +31,7 @@ export class HeroSearchComponent implements OnInit {
   ngOnInit(): void {
     this.heroes$ = this.searchTerms.pipe(
       // wait 300ms after each keystroke before considering the term
-      debounceTime(10),
+      debounceTime(300),
 
       // ignore new term if same as previous term
       distinctUntilChanged(),
